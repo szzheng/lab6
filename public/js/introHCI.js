51b2c174cdc5575e8a47b01d25c17da89cc56728
Steven Zheng <szzheng@ucsd.edu>
2016-02-11 03:10:08 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,12 +27,37 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/" + idNumber, addProject);
+	console.log("/project/" + idNumber)
 }
 
+function addProject(result) {
+	console.log(result);
+	var projectHTML = 
+		'<img src="' + result['image'] + '" class="detailsImage">' +
+		'<p><small>' + result['date'] + result['summary'] + '</small></p></a>';
+
+	$("#project" + result['id'] + ".project .details").html(projectHTML);
+}
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette/", changeColor); 
+}
+
+function changeColor(result) {
+	console.log(result);
+
+	var colors = result['colors']["hex"];
+	//console.log(colors);
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+
 }
